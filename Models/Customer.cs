24d4cd72eccdab4b02347b6c7e66e7d508c14a24
Vivek Handa <vivekhandaindore@gmail.com>
2024-11-19 13:24:47 +0530

    Added more props in Customer class

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -4,5 +4,7 @@
     {
         public int CustomerId { get; set; }
         public string CustomerName { get; set; }
+        public string Address { get; set; }
+        public string Pincode { get; set; }
     }
 }
